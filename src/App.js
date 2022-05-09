efb595cd6fc508844b8c3b529d3a6d701bf8a580
Sai Rithwik M <rithwiksaim@gmail.com>
2022-05-09 21:37:14 +0530

    Minor CSS changes in the navbar

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import React from 'react';
 
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
+import CourseReview from "./pages/CourseReview";
 
 function App() {
     return ( 
         <BrowserRouter>
             <Routes>
                 <Route path = "/" element = { <Homepage /> } /> 
-                <Route path = "*" element = { <PageNotFound /> } /> 
+                <Route path = "/courses" element = { <CourseReview /> } />
+                <Route path = "*" element = { <PageNotFound /> } />
             </Routes>
         </BrowserRouter>
     );
