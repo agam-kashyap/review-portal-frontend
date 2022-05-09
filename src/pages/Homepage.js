043955994826118f2a39d2d09f2b5c99bb4fd15a
Sai Rithwik M <rithwiksaim@gmail.com>
2022-05-09 21:34:57 +0530

    Revert "Modified the landing page"
    This reverts commit 5eb9c9b62aade7635f2eba0c1214922d121e2c6a.

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -7,9 +7,8 @@ import {
     useColorModeValue,
     Input,
     Image,
-    Icon
+    Box
 } from '@chakra-ui/react';
-import { FaCheck } from 'react-icons/fa';
 
 import Container from '../components/Container'
 import React from 'react';
@@ -17,111 +16,102 @@ import { secondaryTextColor } from '../styles/darkMode';
 
 export default function Homepage() {
     const {colorMode} = useColorMode()
-    
-    const Feature = ({ children, ...rest }) => {
-        return (
-            <Stack direction={'row'} align={'center'} {...rest}>
-                <Icon as={FaCheck} color={'green.400'} w={5} h={5} />
-                <Text color={useColorModeValue('gray.600', 'gray.400')} fontWeight={600}>
-                    {children}
-                </Text>
-            </Stack>
-        );
-    };
 
     return (
-    <Container>
-        <Stack
-            maxW={'7xl'}
-            minH={'50vh'}
-            py={ {base: 4, sm: 4, md: 8, lg: 16 }}
-            spacing={{ base: 10, lg: 24 }}
-            direction={{ base: 'column', lg: 'row' }}
-            alignItems={'center'}
-        >
-            <Stack spacing={12} mb={{ base: 12, lg: 0 }} flex={2}>
-                <Heading
-                    as={'h2'}
-                    fontSize={{ base: '3xl', sm: '4xl', md: '5xl', lg: '6xl' }}
-                    maxW={'2xl'}
+        <Container>
+            <Stack
+                align={'center'}
+                spacing={"5vw"}
+                direction={{ base: 'column', md: 'column', lg: 'row', xl: 'row' }}
+                w={{
+                    base: "calc(100vw - 15%)",
+                    md: "calc(100vw - 15%)",
+                    lg: "calc(100vw - 15%)",
+                    xl: "calc(100vw - 30%)",
+                    "2xl": "calc(100vw - 30%)"
+                }}
+                mt={12}
+                mb={12}
+            >
+                <Stack 
+                    spacing={{ base: 5, md: 7 }}
+                    w={{
+                        base: "calc(100vw - 16%)",
+                        md: "calc(100vw - 17%)",
+                        lg: "calc(100vw - 17%)",
+                        xl: "calc(100vw - 20%)",
+                        "2xl": "calc(100vw - 25%)"
+                    }}
                 >
-                    <Text
-                        as={'span'}
-                        position={'relative'}
+                    <Heading
+                        lineHeight={1.1}
+                        fontWeight={600}
+                        fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }}
                     >
-                        Review your courses
+                        <Text
+                            as={'span'}
+                            position={'relative'}
+                        >
+                            Review your courses
+                        </Text>
+                        <br />
+                        <Text as={'span'}
+                            bgGradient='linear(to-l, #FF0080, #7928CA)'
+                            bgClip='text'
+                        >
+                            Anonymously!
+                        </Text>
+                    </Heading>
+                    <Text color={secondaryTextColor[colorMode]}>
+                        An open platform to review the courses & professors at IIIT-B. Add Reviews, Suggest courses, rate them and do many more things!
                     </Text>
-                    <br />
-                    <Text as={'span'}
-                        bgGradient='linear(to-l, #FF0080, #7928CA)'
-                        bgClip='text'
+
+                    {/* Things to do
+                    Add placeholder text typing animations to show what they can type
+                    Integrate this with backend */}
+                    <Stack 
+                        spacing={4} 
+                        direction={{ base: 'column', md: 'row' }} 
+                        w={'full'}
+                        alignItems={'center'}
                     >
-                        Anonymously!
-                    </Text>
-                </Heading>
-                <Stack spacing={6}>
-                <Text color={secondaryTextColor[colorMode]} fontSize={{ md: 'lg' }} maxW={'2xl'}>
-                    An archive of ratings for courses offered at IIIT Bangalore and their Professors
-                </Text>
-                    <Stack
-                        direction={{ base: 'column', sm: 'row' }}
-                        spacing={{ base: 6, sm: 12 }}>
-                            <Stack spacing={4}>
-                                <Feature>Anonymous Reviews</Feature>
-                                <Feature>Upvote/Downvote reviews</Feature>
-                            </Stack>
-                            <Stack spacing={4}>
-                            <Feature>Rate Courses</Feature>
-                            <Feature>Rate Professors</Feature>
-                            </Stack>
+                        <Input
+                            type={'text'}
+                            placeholder={'Software Production Engineering'}
+                            color={useColorModeValue('gray.800', 'gray.200')}
+                            borderStyle='solid'
+                            borderColor={useColorModeValue('gray.800', 'gray.800')}
+                            rounded={'full'}
+                            _focus={{
+                                borderStyle:'solid',
+                                borderColor:useColorModeValue('black', 'black'),
+                                rounded:'full',
+                            }}
+                        />
+                        <Button
+                            variant='outline'
+                            rounded={'full'}
+                            color={useColorModeValue('black', 'white')}
+                            flex={'1 0 auto'}
+                            borderColor={useColorModeValue('gray.800', 'gray.800')}
+                            maxW={'250px'}
+                        >
+                            Search
+                        </Button>
                     </Stack>
                 </Stack>
-                {/* Things to do
-                        Add placeholder text typing animations to show what they can type
-                        Integrate this with backend 
-                */}
-                <Stack 
-                    direction={{ base: 'column', sm: 'row' }} 
-                    spacing={8}
-                    alignItems={'center'}
-                >
-                    <Input
-                        type={'text'}
-                        placeholder={'Software Production Engineering'}
-                        color={useColorModeValue('gray.800', 'gray.200')}
-                        _placeholder={{
-                            color:useColorModeValue('gray.500', 'gray.600')
-                        }}
-                        borderStyle='solid'
-                        borderColor={useColorModeValue('gray.800', 'gray.800')}
-                        rounded={'full'}
-                        _focus={{
-                            borderStyle:'solid',
-                            borderColor:useColorModeValue('black', 'black'),
-                            rounded:'full',
-                        }}
-                    />
-                    <Button
-                        variant='outline'
-                        rounded={'full'}
-                        color={useColorModeValue('black', 'white')}
-                        flex={'1 0 auto'}
-                        borderColor={useColorModeValue('gray.800', 'gray.800')}
-                        maxW={'250px'}
-                    >
-                        Search
-                    </Button>
-                </Stack>
+                <Image
+                    alt={'Hero Image'}
+                    fit={'contain'}
+                    w={{ base: '60vw', md: '40vw', lg: '30vw', xl: '30vw' }}
+                    src={
+                        '/hero/undraw_organize_resume_re_k45b.svg'
+                    }
+                />
             </Stack>
-            <Image
-                alt={'Hero Image'}
-                fit={'contain'}
-                w={{ base: '60vw', md: '40vw', lg: '30vw', xl: '30vw' }}
-                src={
-                    '/hero/undraw_organize_resume_re_k45b.svg'
-                }
-            />
-        </Stack>
-    </Container>
+        </Container>
     );
-};
+}
+
+
+
